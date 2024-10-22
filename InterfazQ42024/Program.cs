20548601dbd89bb795a8862789e18f064fb5317d
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-21 18:50:56 -0600

    Implementacion

--- InterfazQ42024/Program.cs
+++ InterfazQ42024/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using InterfazQ42024;
+
+Operacion operacion = new Operacion();
+Console.WriteLine("Ingrese primer número");
+operacion.N1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese segundo número");
+operacion.N2 = Convert.ToDouble(Console.ReadLine());
+
+operacion.Imprimir();
