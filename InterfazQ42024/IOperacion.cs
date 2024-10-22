bfbb720f52a8e11cb64c407dc268cfef2ddd02d2
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-21 18:50:38 -0600

    Creando interfaz Operacion

---
+++ InterfazQ42024/IOperacion.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InterfazQ42024
+{
+    public interface IOperacion
+    {
+        public double Suma();
+        public double Resta();
+        public double Multiplicacion();
+        public double Division();
+    }
+}
